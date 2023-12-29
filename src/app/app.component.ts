14cbf9673c2d0464b5059d6a9bf4b7ea21e493e4
Samson <samson.asefa@tobania.be>
2023-12-29 16:11:19 +0300

    lesson two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, HomeComponent, RouterOutlet],
+  template: ` <main>
+    <header class="brand-name">
+      <img
+        class="brand-logo"
+        src="/assets/logo.svg"
+        alt="logo"
+        aria-hidden="true"
+      />
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-explore';
